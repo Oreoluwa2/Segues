77c1e5f2a05414d5cc8e024c044c9b6faab4ee87
Oreoluwa2 <68120694+Oreoluwa2@users.noreply.github.com>
2020-07-13 23:47:11 -0500

    Add files via upload

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Oreoluwa Omomofe on 7/13/20.
+//  Copyright © 2020 One Summer Chicago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
